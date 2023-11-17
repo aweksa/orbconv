e95e30514f5ac6c7bfbf0f4f26bde9e9a4415972
Zuwuch <zaleksa@protonmail.com>
2023-11-17 23:35:24 +0100

    Dodao header.txt, options.txt, kreaciju orb9.opt

--- orbconv.py
+++ orbconv.py
@@ -1,11 +1,45 @@
 from subprocess import call
 import os
-#call(["ls", "-l"])
+
+
 base_path = "/home/wooyeah/Documents/DSS"
 orbit_path = base_path + "/orbit9"
 conv_path = base_path + "/conv9"
+yark = False
+yark_roc = 0.001
+asteroids = ("20215", "661346", "15543", "11111", "55555", "12576")
+
+
+output = []
+with open("header.txt") as f:
+    header = f.read().splitlines()
+    
+with open("options.txt") as f:
+    options = f.read().splitlines()
+    
+for el in header:
+    output.append(el)
+
+output.append(f'nvz= {len(asteroids)} ; no. Lyapounov exponents')
+for el in asteroids:
+    output.append(el)
+   
+if(yark):
+    options[-2] = "iyark=0; Yarkovsky effect as secular drift in a 0=no 3=yes 1,2=not in use"
+    with open("yarkovsky.dat", "w") as f:
+        f.write(f'{len(asteroids)} {yark_roc}')
+    call(["mv", "yarkovsky.dat", orbit_path])
+    
+for el in options:
+    output.append(el)
+    
+with open("orb9.opt", 'w') as f:
+    for el in output:
+        f.write(el + '\n')
+
+call(["mv", "orb9.opt", orbit_path])    
+    
 os.chdir(orbit_path)
-#asteroids = ("20215", "661346", "15543")
 call([orbit_path + "/orbit9"])
 call(["mv", orbit_path + "/vast.fil", conv_path])
 call(["mv", orbit_path + "/vpla.fil", conv_path])
